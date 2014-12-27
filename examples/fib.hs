b3eaa5a8907af768ed9378785d7f17de978c659e
Joey Hess <joeyh@joeyh.name>
2014-12-27 02:55:30 -0400

    add fib example

---
+++ examples/fib.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE OverloadedStrings, ExtendedDefaultRules #-}
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+import Control.Monad.Shell
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.IO as T
+import Data.Monoid
+default (T.Text)
+
+main :: IO ()
+main = T.writeFile "fib.sh" $ script $
+	takeParameter () >>= fib >>= cmd "echo"
+
+fib :: Var -> Script Var
+fib n = do
+	prev <- new1
+	acc <- new1
+	forCmd (cmd "seq" prev n) $ \_ -> do
+		setVar acc (AVar acc `APlus` AVar prev)
+		setVar prev (AVar acc `AMinus` AVar prev)
+	return acc
+  where
+	new1 = newVarContaining "1" ()
